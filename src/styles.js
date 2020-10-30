f58eacd056dd733e85912db26e43b08732ae0ac3
AliFeyzi78 <zeron1g2k@gmail.com>
2020-10-30 20:21:41 +0200

    ilk commit

---
+++ src/styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+
+const main = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#212121',
+        paddingTop:10,
+    },
+    banner:{
+        flex:1, 
+        flexDirection:'row', 
+        justifyContent:'space-between'
+    },
+    todoText:{
+        // flex:1, 
+        // flexDirection:'row', 
+        // justifyContent:'space-between'
+    },
+    todoCount:{
+        // flex:1, 
+        // flexDirection:'row', 
+        // justifyContent:'space-between'
+    },
+})
+
+export default main;

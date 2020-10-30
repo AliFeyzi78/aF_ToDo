654a5f345ed5ece060fb00782975a0d64e73e487
AliFeyzi78 <zeron1g2k@gmail.com>
2020-10-30 20:29:37 +0200

    1.commit

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {SafeAreaView,  StyleSheet,  ScrollView,  View,  Text, } from 'react-native';
 
 import styles from './styles'
+
 const Main = () => {
   return (
     <>

9cd25991857bf48afa5303dab9ff076531da52d1
AliFeyzi78 <zeron1g2k@gmail.com>
2020-11-01 23:56:28 +0200

    todo done

--- src/Main.js
+++ src/Main.js
@@ -1,57 +1,66 @@
-import React, {useState} from 'react';
-import {SafeAreaView,  StyleSheet,  ScrollView,  View,  Text, FlatList, KeyboardAvoidingView} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View, KeyboardAvoidingView, FlatList } from 'react-native';
 
-import {main} from './styles';
-import {TodoInput, TodoCard} from './components';
+import { main } from './styles';
+import { TodoInput, TodoCard } from './components';
 
 const Main = () => {
+    const [list, setList] = useState([])
 
-  const [list, setList] = useState([]);
+    function addTodo(text) {
+        const element = {
+            id: list.length,
+            todo: text,
+            isDone: false
+        }
+        const newArray = [element, ...list]
+        setList(newArray);
+    }
+    
+    function doneTodo(todoId) {
+        const newArray = [...list];
+        const todoIndex = newArray.findIndex(item => item.id == todoId);
 
-  function addTodo (text) {
-      const element = {
-        id: list.length,
-        todo: text,
-        isDone: false,
-      }
-      const newArray = [element, ...list]
-      // newArray.push(element)
-      setList(newArray);
-      console.log(newArray);
-  }
+        newArray[todoIndex].isDone = !newArray[todoIndex].isDone;
 
-  const renderTodo = ({item}) => <TodoCard data={item}/>
+        setList(newArray);
+    }
 
-  return (
-    <>
-      <SafeAreaView style={main.container}>
-          <KeyboardAvoidingView style={main.container}>
+    function removeTodo(todoId){
+        const newArray = [...list];
+        const todoIndex = list.findIndex(t=>t.id==todoId);
+        newArray.splice(todoIndex, 1);
+        setList(newArray);
+    }
 
-              <View style={main.banner}>
+    const renderTodo = ({ item }) => {
+        return (
+            <TodoCard
+                data={item}
+                onDone={() => doneTodo(item.id)}
+                onRemove={() => removeTodo(item.id)}
+            />
+        )
+    }
 
-                  
-                  <Text style={main.todoText}>TODO</Text>
-                  <Text style={main.todoCount}>{list.length}</Text>
-            
-              </View>
-
-              <FlatList
-                  keyExtractor = {(item, index)=> index.toString()} 
-                  data={list}
-                  renderItem={renderTodo}
-                  ListEmptyComponent={()=> <Text style={main.emptyComponent}>Nothing to do yet!!</Text>}
-              />
-
-              <TodoInput 
-                  onTodoEnter={todoText=>addTodo(todoText)}  
-              />
-
-          </KeyboardAvoidingView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-
-
+    return (
+        <SafeAreaView style={main.container}>
+            <KeyboardAvoidingView style={main.container}>
+                <View style={main.banner}>
+                    <Text style={main.todoText}>TODO</Text>
+                    <Text style={main.todoCount}>{list.filter(t => t.isDone === false).length}</Text>
+                </View>
+                <FlatList
+                    keyExtractor={(item, index) => index.toString()}
+                    data={list}
+                    renderItem={renderTodo}
+                    ListEmptyComponent={() => <Text style={main.emptyComponent}>Nothing to do..</Text>}
+                />
+                <TodoInput
+                    onTodoEnter={todoText => addTodo(todoText)}
+                />
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    );
+}
 export default Main;
--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 
 import {todo_card} from '../styles'
 
 const TodoCard = (props) => {
     return (
-        <View style={todo_card.container}>
-            <Text style={todo_card.text}>🙂{props.data.todo}</Text>
-        </View>
+        <TouchableOpacity 
+            style={todo_card.container}
+            onPress={()=> props.onDone()}
+            onLongPress={()=>props.onRemove()}
+        >
+            <Text style={[
+                todo_card.text,
+                {textDecorationLine: props.data.isDone ? 'line-through' : null}
+                ]}>🙂{props.data.todo}</Text>
+        </TouchableOpacity>
     )
 }
 
